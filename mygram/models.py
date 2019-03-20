b3f12c493fd89b85ceb5e34373214e68c0d91a81
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-20 11:08:56 +0200

    create Image model in models.py

--- mygram/models.py
+++ mygram/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Image(models.Model):
+     name=models.CharField(max_length=100)
+     image=models.ImageField(upload_to = 'images/')
+     caption=models.TextField()
+     likes=models.IntegerField()
+     
+
 # Create your models here.

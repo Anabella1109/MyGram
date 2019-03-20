fe21b7b66d5861023eb86814155f98d414ae5bed
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-20 15:41:35 +0200

    connect to database

--- mygram/models.py
+++ mygram/models.py
@@ -4,16 +4,21 @@ from django.db import models
 class Profile(models.Model):
     photo=models.ImageField(upload_to='dps/')
     bio=models.TextField()
-class Comment(models.Model):
-    comment=models.TextField()
 
 class Image(models.Model):
      name=models.CharField(max_length=100)
      image=models.ImageField(upload_to = 'images/')
      caption=models.TextField()
      likes=models.IntegerField()
-     comment=models.ForeignKey(Comment)
+     
      profile=models.ForeignKey(Profile)
+
+     def save_image()
+class Comment(models.Model):
+    comment=models.TextField()
+    image=models.ForeignKey(Image,default='timezone.now')
+
+
 
 
 # Create your models here.

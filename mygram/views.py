24bb756654cf84a7d09f2a59879a7ad130062efd
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-20 10:54:39 +0200

    create first view function to view home page in views.py

--- mygram/views.py
+++ mygram/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse,Http404
+
+def home(request):
+     title='Home | MyGram'
+     return render(request,'grams/home.html',{'title':title})
+    
 
 # Create your views here.

ff2695d4b1cf1aab7749f4f86cb4e9af546579aa
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-03-20 11:11:31 +0200

    add Comment model and forign key in Image model in models.py

--- mygram/models.py
+++ mygram/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+class Comment(models.Model):
+    comment=models.TextField()
+
 class Image(models.Model):
      name=models.CharField(max_length=100)
      image=models.ImageField(upload_to = 'images/')
      caption=models.TextField()
      likes=models.IntegerField()
-     
+     comment=models.ForeignKey(Comment)
+
 
 # Create your models here.
